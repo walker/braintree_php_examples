222e6cdfc48bcae141fdddcb2ecda8ecd25da8a2
Braintree <code@getbraintree.com>
2010-05-17 16:53:36 -0500

    updated examples for 2.2.0

---
+++ braintree-php-2.2.0/tests/unit/ResourceCollectionTest.php
@@ -0,0 +1,50 @@
+<?php
+require_once realpath(dirname(__FILE__)) . '/../TestHelper.php';
+
+class Braintree_TestResource
+{
+    static function fetch($ids)
+    {
+        $lookup = function($id) {
+            return Braintree_ResourceCollectionTest::$values[intval($id)];
+        };
+
+        return array_map($lookup, $ids);
+    }
+}
+
+class Braintree_ResourceCollectionTest extends PHPUnit_Framework_TestCase
+{
+    public static $values = array("a", "b", "c", "d", "e");
+
+    function testIterateOverResults()
+    {
+
+        $response = array(
+            'searchResults' => array(
+                'pageSize' => 2,
+                'ids' => array('0', '1', '2', '3', '4')
+            )
+        );
+
+        $pager = array(
+            'className' => 'Braintree_TestResource',
+            'classMethod' => 'fetch',
+            'methodArgs' => array()
+        );
+
+        $collection = new Braintree_ResourceCollection($response, $pager);
+
+        $count = 0;
+        $index = 0;
+        foreach ($collection as $value)
+        {
+            $this->assertEquals(Braintree_ResourceCollectionTest::$values[$index], $value);
+            $index += 1;
+            $count += 1;
+        }
+
+        $this->assertEquals(5, $count);
+    }
+}
+?>
